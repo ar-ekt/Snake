974285458ad24f6ce53fc865d8a83e3135b77e9e
Amirreza Ektesabi <57006850+ar-ekt@users.noreply.github.com>
2020-09-11 17:24:34 +0430

    delete alphasize and add it to two_player file

--- game/berlin_wall.py
+++ game/berlin_wall.py
@@ -21,10 +21,6 @@ def is_round_over(snakes, wall_movement, screen_size):
         return -1
 
 def show(snakes, coins, wall_movement, names, scores, color_p, cell_size):
-    alphasize = {'a': 16, 'b': 20, 'c': 16, 'd': 17, 'e': 19, 'f': 13, 'g': 17, 'h': 18, 'i': 9,
-             'j': 8,'k': 18, 'l': 10, 'm': 25, 'n': 18, 'o': 18, 'p': 18, 'q': 17, 'r': 14,
-             's': 15, 't': 10,'u': 20, 'v': 21, 'w': 21, 'x': 18, 'y': 21, 'z': 17, '0': 16,
-             '1': 12, '2': 18, '3': 17, '4': 15, '5': 17, '6': 15, '7': 15, '8': 15, '9': 17}
     color_red = (120,0,0)
     
     scr()
